82dd96dcc502bd94b2d2722ee4c4cde3f55e0d19
Sharp <rsha7868@740s-pl-127.local>
2017-11-13 12:21:28 -0700

    Initial commit

---
+++ src/gui/view/GUIPanel.java
@@ -0,0 +1,16 @@
+package gui.view;
+import javax.swing.JPanel;
+import gui.controller.GUIAppController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+public class GUIPanel extends JPanel
+{
+	private GUIAppController appController;
+	
+	public GUIPanel(GUIAppController appController)
+	{
+		super();
+		this.appController = appController;
+	}
+}
